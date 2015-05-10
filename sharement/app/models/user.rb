ae8e5cab1f76e6bc2b5951187016d8cfe683ab73
Nya Yeanafehn <Nyeanafehn@gmail.com>
2015-05-09 20:05:51 -0400

    Sessions created sortof

--- sharement/app/models/user.rb
+++ sharement/app/models/user.rb
@@ -2,12 +2,11 @@ class User < ActiveRecord::Base
 	has_and_belongs_to_many :agreements
 	has_secure_password
 
-	# BASE_URL = https://svcs.sandbox.paypal.com/AdaptiveAccounts/CreateAccount
+	# BASE_URL = 'https://svcs.sandbox.paypal.com/AdaptiveAccounts/CreateAccount'
 
-	# def self.sign_in(name)
-		
-
-	# end
+	# def self.signIn(name)
+	# 	url = URI.encode(BASE_URL + name)
+	# end	
 
 
 end
